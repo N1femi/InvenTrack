80b73f78eb66297c5d8131c5183e44a9a99b85d1
N1femi <niffybabs@gmail.com>
2025-11-25 18:14:00 -0500

    Added React backbone to AddProduct.js Component, plan to make it look like card and keep styling consistent across project.

--- src/pages/AddProduct.js
+++ src/pages/AddProduct.js
@@ -1,7 +1,44 @@
+import { useState } from "react"
+
 const AddProduct = () => {
+  const [name, setName] = useState('Item')
+  const [descrip, setDescrip] = useState('')
+  const [quantity, setQuantity] = useState(1)
+  const [errorMsg, setErrorMsg] = useState(null)
+  
+  
   return (
     <div className="page addproduct">
-      <h2>Add Item</h2>
+      <form>
+        <label htmlFor="name">Name:</label>
+        <input
+          type="text"
+          id="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+
+        <label htmlFor="descrip">Description:</label>
+        <input
+          type="text"
+          id="descrip"
+          value={descrip}
+          onChange={(e) => setDescrip(e.target.value)}
+        />
+
+        <label htmlFor="quantity">Quantity:</label>
+        <input
+          type="number"
+          id="quantity"
+          value={quantity}
+          onChange={(e) => setQuantity(e.target.value)}
+        />
+
+        <button>Add New Item</button>
+
+        {errorMsg && <p className="error">{errorMsg}</p>}
+
+      </form>
     </div>
   )
 }
